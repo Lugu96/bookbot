f5442549d282cb87e1ab8e98b6082c7e20c1a350
Lugu96 <gudmundssonludwig@gmail.com>
2024-03-06 05:22:09 +0100

    finished bookbot project

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 def main():
+    from operator import itemgetter, attrgetter
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
-    print(chars_dict)
-
-
+    chars_list = [{"character":key, "count": value} for key, value in chars_dict.items() if key.isalpha()]
+    sorted_list = sorted(chars_list, key=itemgetter("count"), reverse=True)
+    i = 0
+    print(f"--- Begin report of {book_path}---")
+    print(f"{num_words} words found in the document")
+    for item in sorted_list:
+        
+        print(f"the '{sorted_list[i]["character"]}' character was found {sorted_list[i]["count"]} times")
+        i+=1
+    print("end report")
 def get_num_words(text):
     words = text.split()
     return len(words)
-
-
+    
 def get_chars_dict(text):
     chars = {}
     for c in text:
@@ -20,6 +27,8 @@ def get_chars_dict(text):
         else:
             chars[lowered] = 1
     return chars
+
+    
 
 
 def get_book_text(path):
